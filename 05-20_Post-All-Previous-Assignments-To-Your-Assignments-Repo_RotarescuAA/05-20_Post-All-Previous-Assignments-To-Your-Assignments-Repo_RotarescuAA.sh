489ebff018ae68d86b53659f541dd9b1a76ef586
arotare1 <arotare1@jhu.edu>
2017-10-10 11:50:32 -0400

    Assignments for previous weeks

--- 05-20_Post-All-Previous-Assignments-To-Your-Assignments-Repo_RotarescuAA/05-20_Post-All-Previous-Assignments-To-Your-Assignments-Repo_RotarescuAA.sh
+++ 05-20_Post-All-Previous-Assignments-To-Your-Assignments-Repo_RotarescuAA/05-20_Post-All-Previous-Assignments-To-Your-Assignments-Repo_RotarescuAA.sh
@@ -7,7 +7,7 @@ git remote add origin https://github.com/arotare1/Methods-Assignments.git #name 
 git push -u origin master #-u:remembers parameters, origin is name of remote repo and master is the branch
 
 #copy all assignments into Assignments directory in git directory
-cp -r /home/methods/Dropbox/pri/Methods/Assignments/0* /home/methods/Dropbox/git/Methods/Assignments
+cp -r /home/methods/Dropbox/Assignments-Turned-In/Latest/ByStudent/RotarescuAA/0* /home/methods/Dropbox/git/Methods/Assignments
 
 git add /home/methods/Dropbox/git/Methods/Assignments/0*
 git commit -m "Assignments for previous weeks"
